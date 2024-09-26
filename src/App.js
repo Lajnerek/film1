033f1cc6ba5fad9db038be9a27d788923e7a32bc
DESKTOP-Q6I86P3\Grzywaczewski 3TP <testydama@wp.pl>
2024-09-26 12:22:49 +0200

    pole edycyjne i jego etykieta o treści „Tytuł filmu”

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" integrity="sha256-2TnSHycBDAm2wpZmgdi0z81kykGPJAkiUY+Wf97RbvY=" crossorigin="anonymous"></link>
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Tytuł Filmu </label>
+          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+      </form>
     </div>
   );
 }
